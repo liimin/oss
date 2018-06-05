71ea39788dae6431a53e805828d520431e752340
liimin <17653212@qq.com>
2018-06-05 16:13:06 +0800

    commit

---
+++ src/components/Logo/index.vue
@@ -0,0 +1,37 @@
+<template>
+    <h1>
+        <a :href="linkTo" :class="logoClass">
+        </a>
+    </h1>
+</template>
+
+<script>
+    export default {
+      name: 'logo',
+      props: {
+        className: {
+          type: String
+        },
+        linkTo: {
+          type: String,
+          default: '/'
+        }
+      },
+      computed: {
+        logoClass() {
+          return this.className || 'logo'
+        }
+      }
+    }
+</script>
+
+<style scoped>
+  .logo{
+    background: url(../../assets/control_panel_logo.png) 0 0 no-repeat;
+    display: block;
+    width: 256px;
+    height: 58px;
+    overflow: hidden;
+    text-indent: -9999px;
+  }
+</style>
